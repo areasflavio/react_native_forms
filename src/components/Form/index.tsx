08c43c22449679679b14dc57c2bbb605b0d3deb3
Flávio Arêas <vflavio9@gmail.com>
2022-10-16 11:21:30 -0300

    feat: create form with controlled inputs

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,27 +1,56 @@
+import { useForm } from 'react-hook-form';
+
 import { Button } from '../Button';
-import { Input } from '../Input';
+import { ControlledInput } from '../ControlledInput';
 
 import { Container } from './styles';
 
-const VALID_EMAIL_EXPRESSION =
-  /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+};
 
 export function Form() {
-  function handleUserRegister() {}
+  const { control, handleSubmit } = useForm<FormData>();
+
+  const handleUserRegister = (data: FormData) => {
+    console.log(data);
+  };
 
   return (
     <Container>
-      <Input icon="user" placeholder="Nome" />
-      <Input
+      <ControlledInput
+        name="name"
+        control={control}
+        icon="user"
+        placeholder="Nome"
+      />
+      <ControlledInput
+        name="email"
+        control={control}
         icon="mail"
         placeholder="E-mail"
         keyboardType="email-address"
         autoCapitalize="none"
       />
-      <Input icon="lock" placeholder="Senha" secureTextEntry />
-      <Input icon="lock" placeholder="Confirme a senha" secureTextEntry />
+      <ControlledInput
+        name="password"
+        control={control}
+        icon="lock"
+        placeholder="Senha"
+        secureTextEntry
+      />
+      <ControlledInput
+        name="password_confirm"
+        control={control}
+        icon="lock"
+        placeholder="Confirme a senha"
+        secureTextEntry
+      />
 
-      <Button title="Cadastrar" onPress={handleUserRegister} />
+      <Button title="Cadastrar" onPress={handleSubmit(handleUserRegister)} />
     </Container>
   );
 }
