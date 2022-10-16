54ec970be795d46bb2e89c508ffc554fc441b289
Flávio Arêas <vflavio9@gmail.com>
2022-10-16 10:48:25 -0300

    feat: create sign screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { SignUp } from './src/screens/SignUp';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
+    <>
+      <SignUp />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import { Container, Subtitle, Title } from './styles';
+
+export function Header() {
+  return (
+    <Container>
+      <Title>Crie sua{'\n'}conta</Title>
+
+      <Subtitle>
+        Faça seu cadastro de{'\n'}
+        forma rápida e fácil
+      </Subtitle>
+    </Container>
+  );
+}
